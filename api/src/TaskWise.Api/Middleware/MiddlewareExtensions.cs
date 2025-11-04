d8ab077053200d606b041ea692816cc6f27bf1af
NVishwanath215347_brh <NVishwanath@bioreference.com>
2025-11-04 09:47:54 +0530

    Global exception handling added

---
+++ api/src/TaskWise.Api/Middleware/MiddlewareExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskWise.Api.Middleware;
+
+public static class MiddlewareExtensions
+{
+    public static IApplicationBuilder UseGlobalExceptionHandler(this WebApplication app)
+        => app.UseMiddleware<ExceptionMiddleware>();
+}
