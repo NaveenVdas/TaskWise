cc2f0ed6ec920a88ce73647c029fd9f32db9be1d
NVishwanath215347_brh <NVishwanath@bioreference.com>
2025-11-04 09:24:03 +0530

    Logging services configured

--- api/src/TaskWise.Api/Common/Extensions/WebApplicationExtensions.cs
+++ api/src/TaskWise.Api/Common/Extensions/WebApplicationExtensions.cs
@@ -1,9 +1,20 @@
-﻿namespace TaskWise.Api.Common.Extensions;
+﻿using Serilog;
+using Serilog.Context;
+
+namespace TaskWise.Api.Common.Extensions;
 
 public static class WebApplicationExtensions
 {
     public static WebApplication ConfigurePipeline(this WebApplication app)
     {
+        app.Use(async (context, next) =>
+        {
+            using (LogContext.PushProperty("CorrelationId", context.TraceIdentifier))
+            {
+                await next(context);
+            }
+        });
+
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
@@ -11,7 +22,7 @@ public static class WebApplicationExtensions
         }
 
         app.UseHttpsRedirection();
-
+        app.UseSerilogRequestLogging();
         app.UseAuthorization();
 
         app.MapControllers();
--- api/src/TaskWise.Api/Program.cs
+++ api/src/TaskWise.Api/Program.cs
@@ -2,6 +2,7 @@ using TaskWise.Api.Common.Extensions;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+builder.Host.ConfigureApplicationLogging();
 builder.Services.AddApiServices();
 builder.Services.AddSwaggerGen();
 
